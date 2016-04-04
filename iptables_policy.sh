16cd575f210c603ff501061706fb4aeb3329efcd
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-04-04 23:15:47 +0300

    Modified iptables files and added security_groups.py as in OpenStack

--- iptables_policy.sh
+++ iptables_policy.sh
@@ -16,16 +16,15 @@ LAN_NET="192.168.0.0/24"
 PROXY_NET="172.16.0.0/24"
 
 # Packet marks per interface
-hMARK_OUT_LAN2LOCAL="0x4" #0b00100
-hMARK_OUT_LAN2WAN="0x6"   #0b00110
-hMARK_OUT_LAN2CES="0x7"   #0b00111
-hMARK_IN_WAN2LOCAL="0x18" #0b11000
-hMARK_IN_WAN2LAN="0x19"   #0b11001
-hMARK_IN_CES2LAN="0x1d"   #0b11101
-
-hMARK_INOUT_MASK="0x10"   #0b10000
-hMARK_OUT_MASK="0x00"     #0b00000
-hMARK_IN_MASK="0x10"      #0b10000
+hMARK_EGRESS_to_CES="0x4"       #0b00100
+hMARK_EGRESS_to_WAN="0x6"       #0b00110
+hMARK_EGRESS_to_PROXY="0x7"     #0b00111
+hMARK_INGRESS_from_CES="0x18"   #0b11000
+hMARK_INGRESS_from_WAN="0x19"   #0b11001
+hMARK_INGRESS_from_PROXY="0x1d" #0b11101
+hMARK_MASK="0x10"               #0b10000
+hMARK_EGRESS_MASK="0x00"        #0b00000
+hMARK_INGRESS_MASK="0x10"       #0b10000
 
 
 ## Enable NAT
@@ -33,14 +32,14 @@ iptables -t nat -A POSTROUTING -s $LAN_NET -j MASQUERADE
 
 ## Packet clasification with mark in MANGLE table
 ### LAN & WAN
-iptables -t mangle -A FORWARD -i $LAN_NIC   -s $LAN_NET ! -d $PROXY_NET -j MARK --set-mark $hMARK_OUT_LAN2WAN
-iptables -t mangle -A FORWARD -i $WAN_NIC ! -s $PROXY_NET -d $LAN_NET   -j MARK --set-mark $hMARK_IN_WAN2LAN
+iptables -t mangle -A FORWARD -i $LAN_NIC   -s $LAN_NET ! -d $PROXY_NET -j MARK --set-mark $hMARK_EGRESS_to_WAN
+iptables -t mangle -A FORWARD -i $WAN_NIC ! -s $PROXY_NET -d $LAN_NET   -j MARK --set-mark $hMARK_INGRESS_from_WAN
 ### LAN & CES
-iptables -t mangle -A FORWARD -i $LAN_NIC   -s $LAN_NET   -d $PROXY_NET -j MARK --set-mark $hMARK_OUT_LAN2CES
-iptables -t mangle -A FORWARD -i $VTEP_NIC  -s $PROXY_NET -d $LAN_NET   -j MARK --set-mark $hMARK_IN_CES2LAN
+iptables -t mangle -A FORWARD -i $LAN_NIC   -s $LAN_NET   -d $PROXY_NET -j MARK --set-mark $hMARK_EGRESS_to_PROXY
+iptables -t mangle -A FORWARD -i $VTEP_NIC  -s $PROXY_NET -d $LAN_NET   -j MARK --set-mark $hMARK_INGRESS_from_PROXY
 ### LAN & WAN via CES input
-iptables -t mangle -A INPUT   -i $LAN_NIC                               -j MARK --set-mark $hMARK_OUT_LAN2LOCAL
-iptables -t mangle -A INPUT   -i $WAN_NIC                               -j MARK --set-mark $hMARK_IN_WAN2LOCAL
+iptables -t mangle -A INPUT   -i $LAN_NIC                               -j MARK --set-mark $hMARK_EGRESS_to_CES
+iptables -t mangle -A INPUT   -i $WAN_NIC                               -j MARK --set-mark $hMARK_INGRESS_from_CES
 
 
 # doREJECT table - https://wiki.archlinux.org/index.php/Simple_stateful_firewall
@@ -79,28 +78,28 @@ iptables -A CES_POLICY -j CES_POLICY_HTTP
 
 ## Add rules to service chains
 ### CES_POLICY_DNS chain
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p udp --dport 53 -m limit --limit 3/s --limit-burst 5 -j RETURN
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS LAN: " --log-level 7
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p udp --dport 53 -j doREJECT
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p tcp --dport 53 -m limit --limit 3/s --limit-burst 5 -j RETURN
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS LAN: " --log-level 7
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p tcp --dport 53 -j doREJECT
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_IN_WAN2LOCAL -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_IN_WAN2LOCAL -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_IN_WAN2LOCAL -p udp --dport 53 -j doREJECT
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp --dport 53 -j doREJECT
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp --dport 53 -m limit --limit 3/s --limit-burst 5 -j RETURN
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS LAN: " --log-level 7
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp --dport 53 -j doREJECT
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p tcp --dport 53 -m limit --limit 3/s --limit-burst 5 -j RETURN
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS LAN: " --log-level 7
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p tcp --dport 53 -j doREJECT
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p udp --dport 53 -j doREJECT
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 53 -j doREJECT
 iptables -A CES_POLICY_DNS -j RETURN
 ### CES_POLICY_DHCP chain
-iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_OUT_LAN2LOCAL -p udp --sport 68 --dport 67 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_OUT_LAN2LOCAL -p udp --sport 68 --dport 67 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DHCP WAN: " --log-level 7
-iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_OUT_LAN2LOCAL -p udp --sport 68 --dport 67 -j doREJECT
+iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_EGRESS_to_CES -p udp --sport 68 --dport 67 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_EGRESS_to_CES -p udp --sport 68 --dport 67 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DHCP WAN: " --log-level 7
+iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_EGRESS_to_CES -p udp --sport 68 --dport 67 -j doREJECT
 iptables -A CES_POLICY_DHCP -j RETURN
 ### CES_POLICY_HTTP chain
-iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp -m state --state RELATED,ESTABLISHED -j RETURN
-iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp --dport 80  -m state --state NEW     -j RETURN
-iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp --dport 443 -m state --state NEW     -j RETURN
+iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp -m state --state RELATED,ESTABLISHED -j RETURN
+iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 80  -m state --state NEW     -j RETURN
+iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 443 -m state --state NEW     -j RETURN
 
 
 
@@ -110,10 +109,10 @@ iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_IN_WAN2LOCAL -p tcp --dport 44
 iptables -N HOST_POLICY
 iptables -N HOST_192.168.0.101
 iptables -N HOST_192.168.0.102
-iptables -A HOST_POLICY -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -s 192.168.0.101 -j HOST_192.168.0.101
-iptables -A HOST_POLICY -m mark --mark $hMARK_IN_MASK/$hMARK_INOUT_MASK  -d 192.168.0.101 -j HOST_192.168.0.101
-iptables -A HOST_POLICY -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -s 192.168.0.102 -j HOST_192.168.0.102
-iptables -A HOST_POLICY -m mark --mark $hMARK_IN_MASK/$hMARK_INOUT_MASK  -d 192.168.0.102 -j HOST_192.168.0.102
+iptables -A HOST_POLICY -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -s 192.168.0.101 -j HOST_192.168.0.101
+iptables -A HOST_POLICY -m mark --mark $hMARK_INGRESS_MASK/$hMARK_MASK  -d 192.168.0.101 -j HOST_192.168.0.101
+iptables -A HOST_POLICY -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -s 192.168.0.102 -j HOST_192.168.0.102
+iptables -A HOST_POLICY -m mark --mark $hMARK_INGRESS_MASK/$hMARK_MASK  -d 192.168.0.102 -j HOST_192.168.0.102
 
 
 ### EXAMPLE ###
@@ -125,31 +124,31 @@ iptables -N HOST_192.168.0.101_SERVICE2
 
 ## Set host DNS policy
 ### Example of traffic type OUT for DNS over UDP
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -p udp --dport 53 -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp --dport 53 -j doREJECT
 ### Example of traffic type LAN2LOCAL for DNS over UDP - DNS requests to CES
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2LOCAL -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2LOCAL -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2LOCAL -p udp --dport 53 -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_CES -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_CES -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_CES -p udp --dport 53 -j doREJECT
 ### Example of traffic type LAN2WAN for DNS over UDP - DNS requests to public DNS server
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2WAN -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2WAN -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2WAN -p udp --dport 53 -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p udp --dport 53 -j doREJECT
 ### Example of traffic type LAN2WAN for SSH over TCP - Connecting to public SSH servers 
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2WAN -p tcp --dport 22 -j doREJECT
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_IN_WAN2LAN  -p tcp --dport 22 -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p tcp --dport 22 -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_WAN  -p tcp --dport 22 -j doREJECT
 
 ## CES traffic - Maybe mark are a bit pedantic here
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2CES -d 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_IN_CES2LAN  -s 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2CES -d 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_IN_CES2LAN  -s 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_PROXY -d 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_PROXY  -s 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_PROXY -d 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_PROXY  -s 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
 
 ## Reject unallocated outgoing traffic to PROXY_NET
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_LAN2CES -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_PROXY -j doREJECT
 ## Accept any other outgoing traffic
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_OUT_MASK/$hMARK_INOUT_MASK -j RETURN
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -j RETURN
 
 ## Define specific host policies per service
 iptables -A HOST_192.168.0.101_SERVICE1 -j RETURN
---
+++ security_groups.py
@@ -0,0 +1,159 @@
+
+# Packet marks per interface
+hMARK_EGRESS_to_CES='0x4'       #0b00100
+hMARK_EGRESS_to_WAN='0x6'       #0b00110
+hMARK_EGRESS_to_PROXY='0x7'     #0b00111
+hMARK_INGRESS_from_CES='0x18'   #0b11000
+hMARK_INGRESS_from_WAN='0x19'   #0b11001
+hMARK_INGRESS_from_PROXY='0x1d' #0b11101
+hMARK_MASK='0x10'               #0b10000
+hMARK_EGRESS_MASK='0x00'        #0b00000
+hMARK_INGRESS_MASK='0x10'       #0b10000
+
+def _direction_xlat(d):
+    if d == 'ANY':
+        return '{}/{}'.format('0x00', '0x00')
+    elif d == 'EGRESS':
+        return '{}/{}'.format(hMARK_EGRESS_MASK, hMARK_MASK)
+    elif d == 'INGRESS':
+        return '{}/{}'.format(hMARK_INGRESS_MASK, hMARK_MASK)
+    elif d == 'EGRESS_to_CES':
+        return '{}'.format(hMARK_EGRESS_to_CES)
+    elif d == 'EGRESS_to_WAN':
+        return '{}'.format(hMARK_EGRESS_to_WAN)
+    elif d == 'EGRESS_to_PROXY':
+        return '{}'.format(hMARK_EGRESS_to_PROXY)
+    elif d == 'INGRESS_from_CES':
+        return '{}'.format(hMARK_INGRESS_from_CES)
+    elif d == 'INGRESS_from_WAN':
+        return '{}'.format(hMARK_INGRESS_from_WAN)
+    elif d == 'INGRESS_from_PROXY':
+        return '{}'.format(hMARK_INGRESS_from_PROXY)
+    else:
+        raise Exception('direction not supported: {}'.format(d))
+
+def _check_direction(direction, source, destination):
+    if direction == 'ANY':
+        assert(source      == '0.0.0.0/0')
+        assert(destination == '0.0.0.0/0')
+    elif 'EGRESS' in direction:
+        assert(source == '0.0.0.0/0')
+    elif 'INGRESS' in direction:
+        assert(destination == '0.0.0.0/0')
+    return True
+
+def _check_proto(proto):
+    assert(proto >= 0) and (proto <= 255)
+    return True
+
+def _check_port(port, proto):
+    assert(proto in [6,17])
+    if isinstance(port, int):
+        assert(port >= 1) and (port <= 65535)
+    elif isinstance(port, str):
+        [a, b] = port.split(':')
+        assert(not(a is '' and b is ''))
+        if a is '': a = 1
+        if b is '': b = 65535
+        assert(int(a) < int(b))
+        assert(int(a) >= 1) and (int(a) <= 65534)
+        assert(int(b) >= 2) and (int(b) <= 65535)
+    return True
+
+def _check_icmpcode(port, proto):
+    assert(proto in [1])
+    if isinstance(port, int):
+        assert(port >= 0) and (port <= 255)
+    elif isinstance(port, str):
+        [a, b] = port.split('/')
+        assert(not(a is '' or b is ''))
+        assert(int(a) >= 0) and (int(a) <= 255)
+        assert(int(b) >= 0) and (int(b) <= 255)
+    return True 
+            
+def _check_extra(extra):
+    if 'ratelimit' in extra:
+        assert(extra['ratelimit'] >= 0)
+    elif 'ratelimit' in extra and 'rateburst' in extra:
+        # Matching only in rateburst is not recommended
+        assert(extra['rateburst'] >= 0)
+    return True
+
+def check_rule(kwrule):
+    # Set default priority value to 0
+    assert(kwrule.setdefault('priority', 0) >= 0)
+    # Set default source to ANY
+    assert(kwrule.setdefault('source', '0.0.0.0/0'))
+    # Set default destination to ANY
+    assert(kwrule.setdefault('destination', '0.0.0.0/0'))
+    # Set default direction to ANY
+    assert(kwrule.setdefault('direction','ANY'))
+    # Validate and translate direction
+    assert(_check_direction(kwrule['direction'], kwrule['source'], kwrule['destination']))
+    kwrule['direction'] = _direction_xlat(kwrule['direction'])
+    # Set default protocol to ANY
+    assert(kwrule.setdefault('protocol', 0) >= 0)
+    assert(_check_proto(kwrule['protocol']))
+    # Validate source and destination ports
+    if 'sport' in kwrule:
+        assert(_check_port(kwrule['sport'], kwrule['protocol']))
+    if 'dport' in kwrule:
+        assert(_check_port(kwrule['dport'], kwrule['protocol']))
+    # Validate type and code for ICMP
+    if 'icmptype' in kwrule:
+        assert(_check_icmptype(kwrule['icmptype'], kwrule['protocol']))
+    # Validate action
+    assert(kwrule['action'] in ['ACCEPT', 'DROP'])
+    # Validate extra
+    if 'extra' in kwrule:
+        assert(_check_extra(kwrule['extra']))
+    
+    # For assert checks
+    return True
+
+
+# Create rule for DNS over UDP
+rule1={'priority': 10, 'direction': 'EGRESS', 'source': '0.0.0.0/0', 'protocol': 17, 'dport': 53, 'action': 'ACCEPT', 'extra': {'ratelimit': 3, 'rateburst': 5}}
+check_rule(rule1)
+# Create rule for DNS over TCP
+rule2={'priority': 20, 'direction': 'EGRESS', 'source': '0.0.0.0/0', 'protocol': 6,  'dport': 53, 'action': 'DROP'}
+check_rule(rule2)
+# Create service for DNS with above rules
+
+group1 = {'priority': 10, 'name': 'DNS', 'uuid': 'uuid0001', 'rules': [rule1, rule2]}
+
+###
+
+# Create rule for ICMP
+rule3={'priority': 10, 'direction': 'EGRESS' , 'destination': '0.0.0.0/0', 'protocol': 1, 'action': 'DROP'}
+rule4={'priority': 10, 'direction': 'INGRESS', 'destination': '0.0.0.0/0', 'protocol': 1, 'action': 'DROP'}
+rule4={'priority': 10, 'destination': '0.0.0.0/0', 'protocol': 1, 'action': 'DROP'}
+check_rule(rule3)
+check_rule(rule4)
+# Create service for ICMP with above rules
+group2 = {'priority': 10, 'name': 'ICMP', 'uuid': 'uuid0002', 'rules': [rule3, rule4]}
+
+###
+
+# Create rule for SSH
+rule5={'priority': 10, 'protocol': 6, 'dport':22, 'action': 'ACCEPT'}
+check_rule(rule5)
+# Create service for SSH with above rules
+group3 = {'priority': 10, 'name': 'SSH', 'uuid': 'uuid0003', 'rules': [rule5]}
+
+###
+
+# Create rule for mySQL
+rule6={'priority': 10, 'protocol': 6, 'dport':3306, 'action': 'DROP'}
+check_rule(rule6)
+
+# Create service for mySQL with above rules
+group4 = {'priority': 10, 'name': 'mySQL', 'uuid': 'uuid0004', 'rules': [rule6]}
+
+
+# Create dictionary to aggregate all security groups
+secgroups = {'uuid0001': group1,
+             'uuid0002': group2,
+             'uuid0003': group3,
+             'uuid0004': group4,
+             }
