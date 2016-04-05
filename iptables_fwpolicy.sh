e34524aa7c3d5fc0b780892d765bada35b927f92
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-04-05 09:50:32 +0300

    Added comments to iptables_fwpolicy

--- iptables_fwpolicy.sh
+++ iptables_fwpolicy.sh
@@ -23,37 +23,39 @@ hMARK_MASK="0x10"               #0b10000
 hMARK_EGRESS_MASK="0x00"        #0b00000
 hMARK_INGRESS_MASK="0x10"       #0b10000
 
+# Create table
+iptables -N doREJECT
 
 # DEFINITION OF FIREWALL POLICIES TO PROTECT FROM ATTACKS
 iptables -N FIREWALL_POLICY
 
 # Force SYN packets check
-iptables -A FIREWALL_POLICY -p tcp ! --syn -m state --state NEW -j doREJECT
+iptables -A FIREWALL_POLICY -p tcp ! --syn -m state --state NEW -j doREJECT -m comment --comment "Force SYN packets check"
 
 # Force Fragments packets check
-iptables -A FIREWALL_POLICY -f -j doREJECT
+iptables -A FIREWALL_POLICY -f -j doREJECT -m comment --comment "Force Fragments packets check"
 
 # XMAS packets
-iptables -A FIREWALL_POLICY -p tcp --tcp-flags ALL ALL -j doREJECT
+iptables -A FIREWALL_POLICY -p tcp --tcp-flags ALL ALL -j doREJECT -m comment --comment "XMAS packets"
 
 # Drop all NULL packets
-iptables -A FIREWALL_POLICY -p tcp --tcp-flags ALL NONE -j doREJECT
+iptables -A FIREWALL_POLICY -p tcp --tcp-flags ALL NONE -j doREJECT -m comment --comment "Drop all NULL packets"
 
 # Drop vulnerable multiport TCP services
 # http://howtonixnux.blogspot.fi/2008/03/iptables-using-multiport.html
 TCP_MULTIPORTS="135,137,138,139"
-iptables -A FIREWALL_POLICY -p tcp -m state --state NEW -m multiport --dports $TCP_MULTIPORTS -j doREJECT
+iptables -A FIREWALL_POLICY -p tcp -m state --state NEW -m multiport --dports $TCP_MULTIPORTS -j doREJECT -m comment --comment "Drop vulnerable multiport TCP services"
 
 
 # Linux Iptables Avoid IP Spoofing And Bad Addresses Attacks
 # http://www.cyberciti.biz/tips/linux-iptables-8-how-to-avoid-spoofing-and-bad-addresses-attack.html
-iptables -A FIREWALL_POLICY -i $VTEP_NIC ! -s $PROXY_NET -j doREJECT
-iptables -A FIREWALL_POLICY -i $LAN_NIC  ! -s $LAN_NET   -j doREJECT
+iptables -A FIREWALL_POLICY -i $VTEP_NIC ! -s $PROXY_NET -j doREJECT -m comment --comment "IP Spoofing"
+iptables -A FIREWALL_POLICY -i $LAN_NIC  ! -s $LAN_NET   -j doREJECT -m comment --comment "IP Spoofing"
 
 ## Drop all other spoofed traffic
 for ip in $SPOOF_IPS
 do
-    iptables -A FIREWALL_POLICY -i $WAN_NIC -s $ip -j doREJECT
+    iptables -A FIREWALL_POLICY -i $WAN_NIC -s $ip -j doREJECT -m comment --comment "IP Spoofing"
 done
 
 
