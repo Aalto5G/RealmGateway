8d7022ff88f4c7c5be2361d661615ac8c4cac728
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-04-12 00:59:46 +0300

    Create CT zone for packet filtering

--- deleteEnvironment.sh
+++ deleteEnvironment.sh
@@ -1,48 +1,56 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "$0 $*"
+    exit 1
+fi
+
 ###############################################################################
 # Remove supporting infrastructure for CES-A & CES-B
 ###############################################################################
 
 # [CES-A]
 ## LAN side
-sudo ip link set dev qbr-int-lana    down
-sudo ip link set dev qve-phy-lana    down
-sudo ip link set dev qve-l2-lana     down
-sudo ip link del qve-phy-lana
-sudo brctl delbr qbr-int-lana
+ip link set dev qbr-int-lana    down
+ip link set dev qve-phy-lana    down
+ip link set dev qve-l2-lana     down
+ip link del qve-phy-lana
+brctl delbr qbr-int-lana
 
 ## WAN side
-sudo ip link set dev qbr-int-wan     down
-sudo ip link set dev qve-phy-wana    down
-sudo ip link set dev qve-l2-wana     down
-sudo ip link del qve-phy-wana
-sudo brctl delbr qbr-int-wan
+ip link set dev qbr-int-wan     down
+ip link set dev qve-phy-wana    down
+ip link set dev qve-l2-wana     down
+ip link del qve-phy-wana
+brctl delbr qbr-int-wan
 
 ## TUN side
-sudo ip link set dev qve-phy-tuna    down
-sudo ip link set dev qve-l2-tuna     down
-sudo ip link del qve-phy-tuna
-sudo ovs-vsctl --if-exists del-br qbr-int-tuna
+ip link set dev qve-phy-tuna    down
+ip link set dev qve-l2-tuna     down
+ip link del qve-phy-tuna
+ovs-vsctl --if-exists del-br qbr-int-tuna
 
 # [CES-B]
 ## LAN side
-sudo ip link set dev qbr-int-lanb    down
-sudo ip link set dev qve-phy-lanb    down
-sudo ip link set dev qve-l2-lanb     down
-sudo ip link del qve-phy-lanb
-sudo brctl delbr qbr-int-lanb
+ip link set dev qbr-int-lanb    down
+ip link set dev qve-phy-lanb    down
+ip link set dev qve-l2-lanb     down
+ip link del qve-phy-lanb
+brctl delbr qbr-int-lanb
 
 ## WAN side
-sudo ip link set dev qbr-int-wan     down
-sudo ip link set dev qve-phy-wanb    down
-sudo ip link set dev qve-l2-wanb     down
-sudo ip link del qve-phy-wanb
-sudo brctl delbr qbr-int-wan
+ip link set dev qbr-int-wan     down
+ip link set dev qve-phy-wanb    down
+ip link set dev qve-l2-wanb     down
+ip link del qve-phy-wanb
+brctl delbr qbr-int-wan
 
 ## TUN side
-sudo ip link set dev qve-phy-tunb    down
-sudo ip link set dev qve-l2-tunb     down
-sudo ip link del qve-phy-tunb
-sudo ovs-vsctl --if-exists del-br qbr-int-tunb
+ip link set dev qve-phy-tunb    down
+ip link set dev qve-l2-tunb     down
+ip link del qve-phy-tunb
+ovs-vsctl --if-exists del-br qbr-int-tunb
 
 
 ###############################################################################
@@ -50,50 +58,50 @@ sudo ovs-vsctl --if-exists del-br qbr-int-tunb
 ###############################################################################
 
 ## LAN side
-sudo ip link set dev qbr-filter-lana down
-sudo ip link set dev qve-l3-lana     down
-sudo ip link set dev l3-lana         down
-sudo ip link del qve-l3-lana
-sudo brctl delbr qbr-filter-lana
+ip link set dev qbr-filter-lana down
+ip link set dev qve-l3-lana     down
+ip link set dev l3-lana         down
+ip link del qve-l3-lana
+brctl delbr qbr-filter-lana
 
 ## WAN side
-sudo ip link set dev qbr-filter-wana down
-sudo ip link set dev qve-l3-wana     down
-sudo ip link set dev l3-wana         down
-sudo ip link del qve-l3-wana 
-sudo brctl delbr qbr-filter-wana
+ip link set dev qbr-filter-wana down
+ip link set dev qve-l3-wana     down
+ip link set dev l3-wana         down
+ip link del qve-l3-wana 
+brctl delbr qbr-filter-wana
 
 ## TUN side
-sudo ip link set dev qbr-filter-tuna down
-sudo ip link set dev qve-l3-tuna     down
-sudo ip link set dev l3-tuna         down
-sudo ip link del qve-l3-tuna
-sudo brctl delbr qbr-filter-tuna
+ip link set dev qbr-filter-tuna down
+ip link set dev qve-l3-tuna     down
+ip link set dev l3-tuna         down
+ip link del qve-l3-tuna
+brctl delbr qbr-filter-tuna
 
 ###############################################################################
 # Remove CES-B configuration
 ###############################################################################
 
 ## LAN side
-sudo ip link set dev qbr-filter-lanb down
-sudo ip link set dev qve-l3-lanb     down
-sudo ip link set dev l3-lanb         down
-sudo ip link del qve-l3-lanb
-sudo brctl delbr qbr-filter-lanb
+ip link set dev qbr-filter-lanb down
+ip link set dev qve-l3-lanb     down
+ip link set dev l3-lanb         down
+ip link del qve-l3-lanb
+brctl delbr qbr-filter-lanb
 
 ## WAN side
-sudo ip link set dev qbr-filter-wanb down
-sudo ip link set dev qve-l3-wanb     down
-sudo ip link set dev l3-wanb         down
-sudo ip link del qve-l3-wanb 
-sudo brctl delbr qbr-filter-wanb
+ip link set dev qbr-filter-wanb down
+ip link set dev qve-l3-wanb     down
+ip link set dev l3-wanb         down
+ip link del qve-l3-wanb 
+brctl delbr qbr-filter-wanb
 
 ## TUN side
-sudo ip link set dev qbr-filter-tunb down
-sudo ip link set dev qve-l3-tunb     down
-sudo ip link set dev l3-tunb         down
-sudo ip link del qve-l3-tunb
-sudo brctl delbr qbr-filter-tunb
+ip link set dev qbr-filter-tunb down
+ip link set dev qve-l3-tunb     down
+ip link set dev l3-tunb         down
+ip link del qve-l3-tunb
+brctl delbr qbr-filter-tunb
 
 
 ###############################################################################
@@ -101,9 +109,9 @@ sudo brctl delbr qbr-filter-tunb
 ###############################################################################
 
 #Create the default namespace
-sudo ln -s /proc/1/ns/net /var/run/netns/default > /dev/null 2> /dev/null
+ln -s /proc/1/ns/net /var/run/netns/default > /dev/null 2> /dev/null
 
 for i in nslana nslanb nswan; do
     ##Remove and create new namespaces
-    sudo ip netns del $i > /dev/null 2> /dev/null
+    ip netns del $i > /dev/null 2> /dev/null
 done
--- iptables_policy.sh
+++ iptables_policy.sh
@@ -9,37 +9,23 @@
 #
 
 # Definition of variables
-VTEP_NIC="cesa-vetp"
-WAN_NIC="cesa-wan"
-LAN_NIC="cesa-lan"
+LAN_NIC="l3-lana"
+WAN_NIC="l3-wana"
+TUN_NIC="l3-tuna"
 LAN_NET="192.168.0.0/24"
+CPOOL_NET="198.18.0.21/32 198.18.0.22/32 198.18.0.23/32"
 PROXY_NET="172.16.0.0/24"
 
 # Packet marks per interface
 hMARK_EGRESS_to_CES="0x4"       #0b00100
 hMARK_EGRESS_to_WAN="0x6"       #0b00110
-hMARK_EGRESS_to_PROXY="0x7"     #0b00111
-hMARK_INGRESS_from_CES="0x18"   #0b11000
+hMARK_EGRESS_to_TUN="0x7"       #0b00111
+hMARK_INGRESS_to_CES="0x18"     #0b11000
 hMARK_INGRESS_from_WAN="0x19"   #0b11001
-hMARK_INGRESS_from_PROXY="0x1d" #0b11101
+hMARK_INGRESS_from_TUN="0x1d"   #0b11101
 hMARK_MASK="0x10"               #0b10000
 hMARK_EGRESS_MASK="0x00"        #0b00000
 hMARK_INGRESS_MASK="0x10"       #0b10000
-
-
-## Enable NAT
-iptables -t nat -A POSTROUTING -s $LAN_NET -j MASQUERADE
-
-## Packet clasification with mark in MANGLE table
-### LAN & WAN
-iptables -t mangle -A FORWARD -i $LAN_NIC   -s $LAN_NET ! -d $PROXY_NET -j MARK --set-mark $hMARK_EGRESS_to_WAN
-iptables -t mangle -A FORWARD -i $WAN_NIC ! -s $PROXY_NET -d $LAN_NET   -j MARK --set-mark $hMARK_INGRESS_from_WAN
-### LAN & CES
-iptables -t mangle -A FORWARD -i $LAN_NIC   -s $LAN_NET   -d $PROXY_NET -j MARK --set-mark $hMARK_EGRESS_to_PROXY
-iptables -t mangle -A FORWARD -i $VTEP_NIC  -s $PROXY_NET -d $LAN_NET   -j MARK --set-mark $hMARK_INGRESS_from_PROXY
-### LAN & WAN via CES input
-iptables -t mangle -A INPUT   -i $LAN_NIC                               -j MARK --set-mark $hMARK_EGRESS_to_CES
-iptables -t mangle -A INPUT   -i $WAN_NIC                               -j MARK --set-mark $hMARK_INGRESS_from_CES
 
 
 # doREJECT table - https://wiki.archlinux.org/index.php/Simple_stateful_firewall
@@ -48,22 +34,53 @@ iptables -A doREJECT -p udp -j REJECT --reject-with icmp-port-unreachable
 iptables -A doREJECT -p tcp -j REJECT --reject-with tcp-reset
 iptables -A doREJECT -j REJECT --reject-with icmp-proto-unreachable
 
+
 # doREJECTnLOG table - https://wiki.archlinux.org/index.php/Simple_stateful_firewall
 iptables -N doREJECTnLOG
-iptables -A doREJECTnLOG -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "doREJECTnLOG: " --log-level 7
+iptables -A doREJECTnLOG -m hashlimit --hashlimit-name doREJECTnLOG --hashlimit-upto 1/sec  --hashlimit-burst 1 --hashlimit-mode srcip,dstip -j LOG --log-prefix "doREJECTnLOG: " --log-level 7
 iptables -A doREJECTnLOG -p udp -j REJECT --reject-with icmp-port-unreachable
 iptables -A doREJECTnLOG -p tcp -j REJECT --reject-with tcp-reset
 iptables -A doREJECTnLOG -j REJECT --reject-with icmp-proto-unreachable
 
 
-# DEFINITION OF BLACK & WHITE LISTS FOR ENHANCED PACKET FILTERING
-iptables -N BLACKLIST_SOURCES
-iptables -N WHITELIST_SOURCES
-## Test
-iptables -A BLACKLIST_SOURCES -s 192.168.0.102 -j doREJECT
+# Configure raw PREROUTING for specific conntrack zones
+## Note: Use conntrack zone 1 for default connection track and zone 2 for packet filtering via qbr-filter-xyz
+iptables -t raw -A PREROUTING -m physdev --physdev-is-in -j CT --zone 2
+iptables -t raw -A PREROUTING -i $LAN_NIC -j CT --zone 1
+iptables -t raw -A PREROUTING -i $WAN_NIC -j CT --zone 1
+iptables -t raw -A PREROUTING -i $TUN_NIC -j CT --zone 1
 
-# DEFINITION OF FIREWALL POLICIES TO PROTECT FROM ATTACKS
-iptables -N FIREWALL_POLICY
+
+## Packet processing for Circular Pool
+### Note: We can rate limit the number of packets that are sent to the Control Plane to prevent DoS
+iptables -t mangle -A PREROUTING -m physdev --physdev-is-in -j ACCEPT
+for ip in $CPOOL_NET
+do
+    iptables -t mangle -A PREROUTING -i $WAN_NIC -d $ip -m conntrack --ctstate NEW -j NFQUEUE --queue-num 10 -m comment --comment "New connection to Circular Pool $ip"
+done
+
+### Note: Create new chain in nat table for Circular Pool connections and DNAT target
+iptables -t nat -N CIRCULAR_POOL
+iptables -t nat -A PREROUTING -m mark ! --mark 0x00 -i $WAN_NIC -j CIRCULAR_POOL -m comment --comment "Continue to Circular Pool DNAT chain"
+### Add rule per private host for DNAT operation
+iptables -t nat -A CIRCULAR_POOL -m mark --mark 0xC0A80065 -i $WAN_NIC -j DNAT --to-destination 192.168.0.101 -m comment --comment "Forward to 192.168.0.101"
+iptables -t nat -A CIRCULAR_POOL -m mark --mark 0xC0A80066 -i $WAN_NIC -j DNAT --to-destination 192.168.0.102 -m comment --comment "Forward to 192.168.0.102"
+iptables -t nat -A CIRCULAR_POOL -m mark --mark 0xC0A80067 -i $WAN_NIC -j DNAT --to-destination 192.168.0.103 -m comment --comment "Forward to 192.168.0.103"
+
+
+## Packet clasification with mark in MANGLE table only in l3-routed interfaces
+iptables -t mangle -A FORWARD -m physdev --physdev-is-in -j ACCEPT -m comment --comment "Traffic from bridged interface"
+### LAN & WAN
+iptables -t mangle -A FORWARD -i $LAN_NIC -o $WAN_NIC -j MARK --set-mark $hMARK_EGRESS_to_WAN    -m comment --comment "Egress LAN to WAN"
+iptables -t mangle -A FORWARD -i $WAN_NIC -o $LAN_NIC -j MARK --set-mark $hMARK_INGRESS_from_WAN -m comment --comment "Ingress WAN to LAN"
+### LAN & CES
+iptables -t mangle -A FORWARD -i $LAN_NIC -o $TUN_NIC -j MARK --set-mark $hMARK_EGRESS_to_TUN    -m comment --comment "Egress LAN to TUN"
+iptables -t mangle -A FORWARD -i $TUN_NIC -o $LAN_NIC -j MARK --set-mark $hMARK_INGRESS_from_TUN -m comment --comment "Ingress TUN to LAN"
+### LAN & WAN via CES input
+iptables -t mangle -A INPUT   -i $LAN_NIC             -j MARK --set-mark $hMARK_EGRESS_to_CES    -m comment --comment "Egress LAN to CES"
+iptables -t mangle -A INPUT   -i $WAN_NIC             -j MARK --set-mark $hMARK_INGRESS_to_CES   -m comment --comment "Ingress WAN to CES"
+iptables -t mangle -A INPUT   -i $TUN_NIC             -j DROP                                    -m comment --comment "Drop incoming traffic from TUN"
+
 
 # DEFINITION OF CES POLICIES AND CHAINS
 
@@ -84,12 +101,12 @@ iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p udp 
 iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p tcp --dport 53 -m limit --limit 3/s --limit-burst 5 -j RETURN
 iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS LAN: " --log-level 7
 iptables -A CES_POLICY_DNS -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -p tcp --dport 53 -j doREJECT
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p udp --dport 53 -j doREJECT
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
-iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 53 -j doREJECT
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_to_CES -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_to_CES -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_to_CES -p udp --dport 53 -j doREJECT
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_to_CES -p tcp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_to_CES -p tcp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS WAN: " --log-level 7
+iptables -A CES_POLICY_DNS -m mark --mark $hMARK_INGRESS_to_CES -p tcp --dport 53 -j doREJECT
 iptables -A CES_POLICY_DNS -j RETURN
 ### CES_POLICY_DHCP chain
 iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_EGRESS_to_CES -p udp --sport 68 --dport 67 -m limit --limit 2/s --limit-burst 3 -j RETURN
@@ -97,9 +114,9 @@ iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_EGRESS_to_CES -p udp --sport 6
 iptables -A CES_POLICY_DHCP -m mark --mark $hMARK_EGRESS_to_CES -p udp --sport 68 --dport 67 -j doREJECT
 iptables -A CES_POLICY_DHCP -j RETURN
 ### CES_POLICY_HTTP chain
-iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp -m state --state RELATED,ESTABLISHED -j RETURN
-iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 80  -m state --state NEW     -j RETURN
-iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dport 443 -m state --state NEW     -j RETURN
+iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_to_CES -p tcp -m state --state RELATED,ESTABLISHED -j RETURN
+iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_to_CES -p tcp --dport 80  -m state --state NEW     -j RETURN
+iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_to_CES -p tcp --dport 443 -m state --state NEW     -j RETURN
 
 
 
@@ -109,10 +126,10 @@ iptables -A CES_POLICY_HTTP -m mark --mark $hMARK_INGRESS_from_CES -p tcp --dpor
 iptables -N HOST_POLICY
 iptables -N HOST_192.168.0.101
 iptables -N HOST_192.168.0.102
-iptables -A HOST_POLICY -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -s 192.168.0.101 -j HOST_192.168.0.101
-iptables -A HOST_POLICY -m mark --mark $hMARK_INGRESS_MASK/$hMARK_MASK  -d 192.168.0.101 -j HOST_192.168.0.101
-iptables -A HOST_POLICY -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -s 192.168.0.102 -j HOST_192.168.0.102
-iptables -A HOST_POLICY -m mark --mark $hMARK_INGRESS_MASK/$hMARK_MASK  -d 192.168.0.102 -j HOST_192.168.0.102
+iptables -A HOST_POLICY -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK  -s 192.168.0.101 -j HOST_192.168.0.101
+iptables -A HOST_POLICY -m mark --mark $hMARK_INGRESS_MASK/$hMARK_MASK -d 192.168.0.101 -j HOST_192.168.0.101
+iptables -A HOST_POLICY -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK  -s 192.168.0.102 -j HOST_192.168.0.102
+iptables -A HOST_POLICY -m mark --mark $hMARK_INGRESS_MASK/$hMARK_MASK -d 192.168.0.102 -j HOST_192.168.0.102
 
 
 ### EXAMPLE ###
@@ -135,18 +152,18 @@ iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_CES -p udp --dpor
 iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p udp --dport 53 -m limit --limit 2/s --limit-burst 3 -j RETURN
 iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p udp --dport 53 -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix "Exceeded DNS host: " --log-level 7
 iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p udp --dport 53 -j doREJECT
-### Example of traffic type LAN2WAN for SSH over TCP - Connecting to public SSH servers 
+### Example of traffic type LAN2WAN for SSH over TCP - Connecting to public SSH servers
 iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_WAN -p tcp --dport 22 -j doREJECT
 iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_WAN  -p tcp --dport 22 -j doREJECT
 
 ## CES traffic - Maybe mark are a bit pedantic here
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_PROXY -d 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_PROXY  -s 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_PROXY -d 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_PROXY  -s 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_TUN    -d 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_TUN -s 172.16.0.1 -j HOST_192.168.0.101_SERVICE1
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_TUN    -d 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_INGRESS_from_TUN -s 172.16.0.2 -j HOST_192.168.0.101_SERVICE2
 
 ## Reject unallocated outgoing traffic to PROXY_NET
-iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_PROXY -j doREJECT
+iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_to_TUN -j doREJECT
 ## Accept any other outgoing traffic
 iptables -A HOST_192.168.0.101 -m mark --mark $hMARK_EGRESS_MASK/$hMARK_MASK -j RETURN
 
@@ -155,41 +172,45 @@ iptables -A HOST_192.168.0.101_SERVICE1 -j RETURN
 iptables -A HOST_192.168.0.101_SERVICE2 -j RETURN
 
 
+# Definition of BLACKLIST & WHITELIST for enhanced packet filtering
+iptables -N BLACKLIST_SOURCES
+iptables -N WHITELIST_SOURCES
+## Test
+iptables -A BLACKLIST_SOURCES -s 192.168.0.102 -j doREJECT
+iptables -A WHITELIST_SOURCES -s 192.168.0.101 -j ACCEPT
+
 
 ## CES LOCAL PROCESS (INPUT chain)
-iptables -A INPUT -j BLACKLIST_SOURCES   # Drop   if source is blacklisted
-iptables -A INPUT -j WHITELIST_SOURCES   # Accept if source is whitelisted
-iptables -A INPUT -j FIREWALL_POLICY     # Return if not an attack
-iptables -A INPUT -j HOST_POLICY         # Return if the host policy was accepted
-iptables -A INPUT -j CES_POLICY          # Return if the CES policy was accepted
-iptables -A INPUT -j ACCEPT              # Accept traffic if not dropped previously
+#iptables -A INPUT -j BLACKLIST_SOURCES                           # Drop   if source is blacklisted
+#iptables -A INPUT -j WHITELIST_SOURCES                           # Accept if source is whitelisted
+#iptables -A INPUT -j FIREWALL_POLICY                             # Return if not an attack
+iptables -A INPUT -m mark ! --mark 0x00 -j HOST_POLICY           # Return if the host policy was accepted
+iptables -A INPUT -m mark ! --mark 0x00 -j CES_POLICY            # Return if the CES policy was accepted
+iptables -A INPUT -j ACCEPT                                      # Accept traffic if not dropped previously
 
 
 ## CES FORWARDING PROCESS (FORWARD chain)
-iptables -A FORWARD -j BLACKLIST_SOURCES # Drop   if source is blacklisted
-iptables -A FORWARD -j WHITELIST_SOURCES # Accept if source is whitelisted
-iptables -A FORWARD -j FIREWALL_POLICY   # Return if not an attack
-iptables -A FORWARD -j HOST_POLICY       # Return if the host policy was accepted
-iptables -A FORWARD -j CES_POLICY        # Return if the CES policy was accepted
-iptables -A FORWARD -j ACCEPT            # Accept traffic if not dropped previously
+iptables -I FORWARD 1 -j BLACKLIST_SOURCES                       # Drop   if source is blacklisted - Insert at position 1
+iptables -I FORWARD 2 -j WHITELIST_SOURCES                       # Accept if source is whitelisted - Insert at position 2
+#iptables -A FORWARD -j FIREWALL_POLICY                           # Return if not an attack
+iptables -A FORWARD -m mark ! --mark 0x00 -j HOST_POLICY         # Return if the host policy was accepted
+iptables -A FORWARD -m mark ! --mark 0x00 -j CES_POLICY          # Return if the CES policy was accepted
+iptables -A FORWARD -j ACCEPT                                    # Accept traffic if not dropped previously
+
+
+## Enable NAT
+#iptables -t nat -A POSTROUTING -m physdev --physdev-is-bridged  -j ACCEPT -m comment --comment "Traffic from bridged interface"
+#iptables -t nat -F
+iptables -t nat -I POSTROUTING -o lo -j ACCEPT
+iptables -t nat -I POSTROUTING -o eth0 -j ACCEPT
+iptables -t nat -I POSTROUTING -o eth1 -j ACCEPT
+
+#iptables -t nat -A POSTROUTING -m mark ! --mark 0x00 -j LOG --log-level 7 --log-prefix "nat.POST: mark!=0 "
+#iptables -t nat -A POSTROUTING -m mark   --mark 0x00 -j LOG --log-level 7 --log-prefix "nat.POST: mark==0 "
+iptables -t nat -A POSTROUTING -m mark --mark $hMARK_EGRESS_to_WAN -s $LAN_NET -j SNAT --to-source 198.18.0.11 -m comment --comment "Outgoing SNAT to 198.18.0.11"
+
 
 
 ###############################################################################################################################################################
 ###############################################################################################################################################################
 ###############################################################################################################################################################
-
-
-## Debugging chains
-## Create LOGnDROP chain
-#iptables -N LOGnDROP
-#iptables -A LOGnDROP -j LOG --log-prefix "LOGnDROP: " --log-level 7
-#iptables -A LOGnDROP -j DROP
-## Create LOGnACCEPT chain
-#iptables -N LOGnACCEPT
-#iptables -A LOGnACCEPT -j LOG --log-prefix "LOGnACCEPT: " --log-level 7
-#iptables -A LOGnACCEPT -j ACCEPT
-## Create LOGnRETURN chain
-#iptables -N LOGnRETURN
-#iptables -A LOGnRETURN -j LOG --log-prefix "LOGnRETURN: " --log-level 7
-#iptables -A LOGnRETURN -j RETURN
-#
