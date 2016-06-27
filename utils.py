025729feefd994359d4dd83da1ad7b2901a362c2
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-06-27 03:04:26 +0300

    Added more functions to utils module

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import socket
 
+def trace():
+    print('Exception in user code:')
+    print('-' * 60)
+    traceback.print_exc(file=sys.stdout)
+    print('-' * 60)
+
 def is_ipv4(ipaddr):
     try:
         assert(socket.inet_pton(socket.AF_INET, ipaddr))
@@ -13,3 +19,12 @@ def is_ipv6(ipaddr):
         return True
     except:
         return False
+
+def set_attributes(obj, **kwargs):
+    for k,v in kwargs.items():
+        #print('setattr({},{})'.format(k, v))
+        setattr(obj, k, v)
+
+def set_default_attributes(obj, args, value=None):
+    for arg in args:
+        setattr(obj, arg, value)
