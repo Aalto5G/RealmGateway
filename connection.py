afe8bc644e2dee9ff11943bc6163a3f90ea27d66
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-07-12 01:07:20 +0300

    Added dns client info to RGW connection

--- connection.py
+++ connection.py
@@ -6,13 +6,35 @@ import time
 LOGLEVELCONNECTIONTABLE = logging.WARNING
 LOGLEVELCONNECTIONENTRY = logging.WARNING
 
-KEY_FQDN = 0
-KEY_SFQDN = 1
+KEY_RGW = 0
+KEY_RGW_FQDN = 0
+KEY_RGW_SFQDN = 1
 
 class ConnectionTable(container3.Container):
     def __init__(self, name='ConnectionTable'):
         """ Initialize as a Container """
         super().__init__(name, LOGLEVELCONNECTIONTABLE)
+    
+    def _update_set(self, s):
+        myset = set(s)
+        for node in myset:
+            if node.hasexpired():
+                self.remove(node)
+    
+    def update_all_rgw(self):
+        conn_set = self.lookup(KEY_RGW, update=False, check_expire=False)
+        if conn_set is None:
+            return
+        self._update_set(conn_set)
+    
+    def get_all_rgw(self, update=True):
+        conn_set = self.lookup(KEY_RGW, update=False, check_expire=False)
+        if conn_set is None:
+            return []
+        if update:
+            self._update_set(conn_set)
+        return conn_set
+
 
 class ConnectionEntryRGW(container3.ContainerNode):
     def __init__(self, name='ConnectionEntryRGW', **kwargs):
@@ -26,8 +48,10 @@ class ConnectionEntryRGW(container3.ContainerNode):
         @type public_port: Integer or None
         @param public_protocol: Allocated public protocol number.
         @type public_protocol: Integer or None
-        @param dns_ipv4: Allocating public IPv4 address of the DNS server.
-        @type dns_ipv4: String
+        @param dns_server_ipv4: IPv4 address of the DNS querying server.
+        @type dns_server_ipv4: String
+        @param dns_client_ipv4: IPv4 address of the DNS originating client.
+        @type dns_client_ipv4: String
         @param host_ipv4: Private IPv4 address of the allocated host.
         @type host_ipv4: String
         @param host_fqdn: Allocating FQDN.
@@ -36,40 +60,52 @@ class ConnectionEntryRGW(container3.ContainerNode):
         @type timeout: Integer or float
         """
         super().__init__(name, LOGLEVELCONNECTIONENTRY)
-        attrlist = ['public_ipv4','public_port','public_protocol','dns_ipv4','host_ipv4','host_fqdn','timeout']
+        attrlist = ['public_ipv4','public_port','public_protocol','dns_server_ipv4','dns_client_ipv4','host_ipv4','host_fqdn','timeout']
         utils.set_default_attributes(self, attrlist, None)
         utils.set_attributes(self, **kwargs)
         self.timestamp_zero = time.time()
         self.timestamp_eol = self.timestamp_zero + self.timeout
+        self._build_lookupkeys()
+
+    def _build_lookupkeys(self):
+        """ Build the lookup keys """
+        # Create specific key if the connection is allocated to an SFQDN domain
+        rgw_key = (KEY_RGW, False)
+        s_fqdn_key = ((KEY_RGW, KEY_RGW_FQDN), False)
+        if (self.public_port, self.public_protocol) != (None, None):
+            s_fqdn_key = ((KEY_RGW, KEY_RGW_SFQDN), False)
+        host_key = ((KEY_RGW, self.host_ipv4), False)
+        public_ipv4_key = ((KEY_RGW, self.public_ipv4), False)
+        conn_key = ((KEY_RGW, self.public_ipv4, self.public_port, self.public_protocol), True)
+        # Create tuple with all calculated keys
+        self._built_lookupkeys = (rgw_key, s_fqdn_key, host_key, public_ipv4_key, conn_key)
 
     def lookupkeys(self):
         """ Return the lookup keys """
-        extra_key = (KEY_FQDN, True)
-        if (self.public_port, self.public_protocol) != (None, None):
-            extra_key = (KEY_SFQDN, True)
-        return (((self.public_ipv4, self.public_port, self.public_protocol), False), extra_key)
+        # Return an iterable (key, isunique)
+        return self._built_lookupkeys
 
     def hasexpired(self):
         """ Return True if the timeout has expired """
         return time.time() > self.timestamp_eol
 
     def __repr__(self):
-        if self.public_port:
-            return '{}:{} -> {}:{} {} ({})'.format(self.public_ipv4, self.public_port, self.host_ipv4,
-                                                   self.public_port, self.public_protocol, self.dns_ipv4)
-        else:
-            return '{} -> {} ({})'.format(self.public_ipv4, self.host_ipv4, self.dns_ipv4)
-
+        return '{}:{} -> {}:{} {} ({}/{})'.format(self.public_ipv4, self.public_port, self.host_ipv4,
+                                                  self.public_port, self.public_protocol,
+                                                  self.dns_server_ipv4, self.dns_client_ipv4)
 
 if __name__ == "__main__":
     table = ConnectionTable()
-    d1 = {'public_ipv4':'1.2.3.4','dns_ipv4':'8.8.8.8','host_ipv4':'192.168.0.100','host_fqdn':'host100.rgw','timeout':2.0}
+    d1 = {'public_ipv4':'1.2.3.4','dns_server_ipv4':'8.8.8.8','host_ipv4':'192.168.0.100','host_fqdn':'host100.rgw','timeout':2.0}
     c1 = ConnectionEntryRGW(**d1)
-    d2 = {'public_ipv4':'1.2.3.5','dns_ipv4':'8.8.8.8','host_ipv4':'192.168.0.100','host_fqdn':'host100.rgw','timeout':2.0,
+    d2 = {'public_ipv4':'1.2.3.5','dns_server_ipv4':'8.8.8.8','host_ipv4':'192.168.0.100','host_fqdn':'host100.rgw','timeout':2.0,
           'public_port':12345,'public_protocol':6}
     c2 = ConnectionEntryRGW(**d2)
     table.add(c1)
     table.add(c2)
+    
+    
+    
     print('Connection c1 has expired?')
     print(c1.hasexpired())
     print(table)
@@ -78,3 +114,5 @@ if __name__ == "__main__":
     time.sleep(3)
     print('Connection c1 has expired?')
     print(c1.hasexpired())
+    
+    table.update_all_rgw()
