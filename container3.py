8b55db6a342a1c1743f2970c277f7a308ff9d6f2
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-07-12 01:02:55 +0300

    Changed interface of container3 module

--- container3.py
+++ container3.py
@@ -18,13 +18,42 @@ class Container(object):
         """
         Initialize the Container.
         """
-        self._version = 0.4
+        self._version = 0.5
         self._logger = logging.getLogger(name)
         self._logger.setLevel(loglevel)
         self._name = name
         self._dict = {}             # Indexes lookup keys to nodes
-        self._list = []             # Stores a list of indexed nodes
+        self._list = set()          # Stores a set of indexed nodes
         self._dict_id2keys = {}     # Indexes node ids to registered keys
+
+    def _add_lookupkeys(self, node, keys):
+        for key, isunique in keys:
+            # The key is not unique - create a set of items for key
+            if not isunique:
+                if key not in self._dict:
+                    self._dict[key] = set()
+                self._dict[key].add(node)
+            # Check the unique key is not already in use
+            elif key in self._dict:
+                raise KeyError('Failed to add: key {} already exists in dictionary for node {}'.format(key, self._dict[key]))
+            # Add the unique key to the dictionary
+            else:
+                self._dict[key] = node
+
+    def _remove_lookupkeys(self, node, keys):
+        for key, isunique in keys:
+            # The key is not unique - remove from set of items for key
+            if not isunique:
+                self._dict[key].remove(node)
+                # The set has no more items, remove set
+                if len(self._dict[key]) == 0:
+                    del self._dict[key]
+            # Check the unique key is already in use
+            elif key not in self._dict:
+                raise KeyError('Failed to remove: key {} does not exists in dictionary for node {}'.format(key, node))
+            # Add the unique key to the dictionary
+            else:
+                del self._dict[key]
 
     def add(self, node):
         """
@@ -33,38 +62,18 @@ class Container(object):
         @param node: The node
         """
         if node in self._list:
-            raise KeyError('Failed to add. Node exists in list %s')
+            raise Exception('Failed to add: node already exists {}'.format(node))
 
-        for key, isalist in node.lookupkeys():
-            # The key is not unique - append to list
-            if isalist:
-                if key not in self._dict:
-                    self._dict[key] = []
-                if node in self._dict[key]:
-                    raise KeyError(
-                        'Failed to add. Node exists in dictionary %s' %
-                        (self._dict[key]))
-                self._dict[key].append(node)
+        # Register node lookup keys
+        keys = node.lookupkeys()
+        self._add_lookupkeys(node, keys)
+        # Map node id to lookup keys
+        self._dict_id2keys[id(node)] = keys
+        # Add node to the set
+        self._list.add(node)
+        self._logger.debug('Added node {}'.format(node))
 
-            # Check the unique key is not already in use
-            elif key in self._dict:
-                raise KeyError(
-                    'Failed to add. Node exists in dictionary %s' %
-                    (self._dict[key]))
-
-            # Add the unique key to the dictionary
-            else:
-                self._dict[key] = node
-
-        # Add node to the list
-        self._list.append(node)
-        self._list.sort(key=lambda x: x.getsortkey())
-        self._logger.debug('Adding <%s>' % (node))
-
-        # Add node lookup keys to a dictionary - Map node id to lookup keys
-        self._dict_id2keys[id(node)] = node.lookupkeys()
-
-    def get(self, key, update=True):
+    def get(self, key, update=False):
         """
         Obtain a node with the given key.
 
@@ -76,13 +85,13 @@ class Container(object):
         if update:
             node.update()
         return node
-    
+
     def getall(self):
         """
-        Returns a shallow copy of the internal list node
+        Returns a shallow copy of the internal set
         """
         return list(self._list)
-    
+
     def has(self, key, check_expire=True):
         """
         Check if there is a node with the given key
@@ -93,13 +102,15 @@ class Container(object):
         """
         try:
             node = self._dict[key]
+            if not isinstance(node, ContainerNode):
+                return True
             if check_expire and node.hasexpired():
                 self.delete(node)
                 return False
             return True
         except KeyError:
             return False
-    
+
     def lookup(self, key, update=True, check_expire=True):
         """
         Look up a node with the given key.
@@ -111,6 +122,8 @@ class Container(object):
         """
         try:
             node = self._dict[key]
+            if not isinstance(node, ContainerNode):
+                return node
             if check_expire and node.hasexpired():
                 self.remove(node)
             if update:
@@ -118,92 +131,55 @@ class Container(object):
             return node
         except KeyError:
             return None
-        
+
     def remove(self, node, callback=True):
         """
         Remove an node from the Storage node.
 
         @param node: The node
         """
-        try:
-            if callback:
-                node.delete()
-            for key, isalist in node.lookupkeys():
-                if isalist:
-                    self._dict[key].remove(node)
-                    # The list has no more nodes, remove
-                    if len(self._dict[key]) == 0:
-                        del self._dict[key]
-                else:
-                    del self._dict[key]
-            self._list.remove(node)
-            del self._dict_id2keys[id(node)]
-            self._logger.debug('Removing <%s>' % (node))
-        except Exception as ex:
-            self._logger.warning(
-                'Failed while removing <%s>: %s' %
-                (node, str(ex)))
-            pass
-        
-    def removeall(self, instance=None):
+        # Remove node lookup keys
+        keys = node.lookupkeys()
+        self._remove_lookupkeys(node, keys)
+        # Remove map node id to lookup keys
+        del self._dict_id2keys[id(node)]
+        # Remove node from the set
+        self._list.remove(node)
+        self._logger.debug('Removed node {}'.format(node))
+        # Evaluate callback to ContainerNode item
+        if callback:
+            self._logger.debug('Delete callback for node {}'.format(node))
+            node.delete()
+
+    def removeall(self, callback=True):
+        # Iterate all nodes in the set and remove them
+        for node in self._list:
+            self.remove(node, callback)
+        # Sanity clear
         self._dict_id2keys.clear()
         self._dict.clear()
-        self._list[:] = []
-
-    
+        self._list.clear()
 
     def updatekeys(self, node):
+        # Get lookup keys
+        old_keys = self._dict_id2keys[id(node)]
+        new_keys = node.lookupkeys()
         # Remove previous keys
-        oldKeys = self._dict_id2keys[id(node)]
-        try:
-            for key, isalist in oldKeys:
-                if isalist:
-                    self._dict[key].remove(node)
-                    # The list has no more nodes, remove
-                    if len(self._dict[key]) == 0:
-                        del self._dict[key]
-                else:
-                    del self._dict[key]
-            del self._dict_id2keys[id(node)]
-            self._logger.debug('Removing keys for <%s>' % (node))
-        except Exception as ex:
-            self._logger.warning(
-                'Failed while removing keys for <%s>: %s' %
-                (node, str(ex)))
-            return
-
-        # Register new keys
-        newKeys = node.lookupkeys()
-        for key, isalist in newKeys:
-            # The key is not unique - append to list
-            if isalist:
-                if key not in self._dict:
-                    self._dict[key] = []
-                if node in self._dict[key]:
-                    raise KeyError(
-                        'Failed to add. Node exists in dictionary %s' %
-                        (self._dict[key]))
-                self._dict[key].append(node)
-
-            # Check the unique key is not already in use
-            elif key in self._dict:
-                raise KeyError(
-                    'Failed to add. Node exists in dictionary %s' %
-                    (self._dict[key]))
-
-            # Add the unique key to the dictionary
-            else:
-                self._dict[key] = node
-
-        # Add node lookup keys to a dictionary - Map node id to lookup keys
-        self._dict_id2keys[id(node)] = newKeys
+        self._remove_lookupkeys(node, old_keys)
+        # Remove map node id to lookup keys
+        del self._dict_id2keys[id(node)]
+        # Register node lookup keys
+        self._add_lookupkeys(node, new_keys)
+        # Map node id to lookup keys
+        self._dict_id2keys[id(node)] = new_keys
+        self._logger.debug('Updated keys for node {}'.format(node))
 
     def __len__(self):
         """ Returns the length of the internal list node """
         return len(self._list)
 
     def __repr__(self):
-        return '\n'.join([str(node) for node in self._list])
+        return '\n'.join(['{}'.format(node) for node in self._list])
 
 
 class ContainerNode(object):
@@ -212,12 +188,13 @@ class ContainerNode(object):
         """ Initialize the ContainerNode """
         self._logger = logging.getLogger(name)
         self._logger.setLevel(loglevel)
+        self._name = name
 
     def lookupkeys(self):
         """ Return the lookup keys of the node """
-        key = ()
-        isalist = False
-        return ((key, isalist),)
+        key = self._name
+        isunique = True
+        return ((key, isunique),)
 
     def hasexpired(self):
         """ Return True if the TTL of the node has expired """
@@ -235,8 +212,11 @@ class ContainerNode(object):
         """
         pass
 
-    def getsortkey(self):
-        """
-        Sort the Container list when an node is added
-        """
-        return 0
+
+if __name__ == "__main__":
+    ct = Container()
+    cn1 = ContainerNode('cn1')
+    ct.add(cn1)
+    cn2 = ContainerNode('cn2')
+    ct.add(cn2)
+    print(ct)
