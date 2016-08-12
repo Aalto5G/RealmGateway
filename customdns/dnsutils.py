7f5462687437b90a1fbbfdf888ff2daa5a0f0836
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-08-12 22:22:58 +0300

    Monkey patched dns.name.Name.to_text() function to avoid bytes() problems

--- customdns/dnsutils.py
+++ customdns/dnsutils.py
@@ -7,6 +7,13 @@ from dns.exception import DNSException
 from dns.rdataclass import *
 from dns.rdatatype import *
 
+#Hack for dnspython (1.14.0)) - Monkey patch to_text() function
+import dns.name
+try:
+    dns.name.Name.to_text = dns.name.Name.to_unicode
+except AttributeError:
+    pass
+
 def debug_data(data):
     return('({} bytes) "{}"'.format(len(data), data))
 
@@ -55,46 +62,3 @@ def make_response_answer_rr(query, name, rdtype, target, rdclass=1, ttl=60):
     response = dns.message.make_response(query, recursion_available=True)
     response.answer = [dns.rrset.from_text(name, ttl, rdclass, rdtype, target)]
     return response
-
-'''
-def _get_dummy(zone):
-    return '{}.{}'.format('__secret', zone.origin.to_text())
-
-def load_zone(zone_file, origin):
-    return dns.zone.from_file(zone_file, origin, relativize=False)
-
-def add_node(zone, name, rdtype, address, ttl=60):
-    import dns.rdtypes.IN.A
-    import dns.rdtypes.ANY.CNAME
-
-    print('Add node {} {}'.format(name, address))
-    assert(rdtype == A)
-
-    # Make dns.name object
-    dnsname = dns.name.from_text(name)
-    if not zone.origin.is_superdomain(dnsname):
-        dnsname = dns.name.from_text(name, zone.origin)
-
-    # Create A record with given IP address
-    rdataset = zone.find_rdataset(dnsname, rdtype, create=True)
-    rdata = dns.rdtypes.IN.A.A(IN, A, address=address)
-    rdataset.add(rdata, ttl=ttl)
-
-    # Create CNAME record for NAPTR lookups
-    target = dns.name.from_text(_get_dummy(zone))
-    rdataset = zone.find_rdataset(dnsname, CNAME, create=True)
-    rdata = dns.rdtypes.ANY.CNAME.CNAME(IN, CNAME, target)
-    rdataset.add(rdata, ttl=ttl)
-
-def delete_node(zone, name):
-    try:
-        print('Delete node {}'.format(name))
-        # Make dns.name object
-        dnsname = dns.name.from_text(name)
-        if not zone.origin.is_superdomain(dnsname):
-            dnsname = dns.name.from_text(name, zone.origin)
-        del zone.nodes[dnsname]
-    except Exception as e:
-        print('Failed to delete node {}: {}'.format(name, e))
-
-'''
