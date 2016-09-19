c03719d04bceb869ef8545255d55e05779967afc
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2016-09-19 13:05:27 +0300

    Moved scripts to old_scripts folder

--- old_scripts/cleanupEnvironment.sh
+++ old_scripts/cleanupEnvironment.sh
@@ -7,101 +7,60 @@ if [[ $UID != 0 ]]; then
 fi
 
 ###############################################################################
-# Remove supporting infrastructure for CES-A & CES-B
-###############################################################################
-
-# [CES-A]
-## LAN side
-ip link set dev qbi-lana     down
-ip link set dev qve-phy-lana down
-ip link set dev qvi-phy-lana down
-ip link del qve-phy-lana
-brctl delbr qbi-lana
-
-## WAN side
-ip link set dev qbi-wan      down
-ip link set dev qve-phy-wana down
-ip link set dev qvi-phy-wana down
-ip link del qve-phy-wana
-brctl delbr qbi-wan
-
-## TUN side
-ip link set dev qve-phy-tuna down
-ip link set dev qvi-phy-tuna down
-ip link del qve-phy-tuna
-ovs-vsctl --if-exists del-br qbi-tuna
-
-# [CES-B]
-## LAN side
-ip link set dev qbi-lanb     down
-ip link set dev qve-phy-lanb down
-ip link set dev qvi-phy-lanb down
-ip link del qve-phy-lanb
-brctl delbr qbi-lanb
-
-## WAN side
-ip link set dev qbi-wan      down
-ip link set dev qve-phy-wanb down
-ip link set dev qvi-phy-wanb down
-ip link del qve-phy-wanb
-brctl delbr qbi-wan
-
-## TUN side
-ip link set dev qve-phy-tunb down
-ip link set dev qvi-phy-tunb down
-ip link del qve-phy-tunb
-ovs-vsctl --if-exists del-br qbi-tunb
-
-
-###############################################################################
 # Remove CES-A configuration
 ###############################################################################
 
 ## LAN side
-ip link set dev qbf-lana    down
-ip link set dev qve-l3-lana down
-ip link set dev l3-lana     down
-ip link del qve-l3-lana
-brctl delbr qbf-lana
+ip link set dev l3-lana down
+ip link del l3-lana
 
 ## WAN side
-ip link set dev qbf-wana     down
-ip link set dev qve-l3-wana  down
-ip link set dev l3-wana      down
-ip link del qve-l3-wana 
-brctl delbr qbf-wana
+ip link set dev l3-wana down
+ip link del l3-wana
 
 ## TUN side
-ip link set dev qbf-tuna     down
-ip link set dev qve-l3-tuna  down
-ip link set dev l3-tuna      down
-ip link del qve-l3-tuna
-brctl delbr qbf-tuna
+ip link set dev l3-tuna down
+ip link del l3-tuna
+ovs-vsctl --if-exists del-br qbi-tuna
+
 
 ###############################################################################
 # Remove CES-B configuration
 ###############################################################################
 
 ## LAN side
-ip link set dev qbf-lanb     down
-ip link set dev qve-l3-lanb  down
-ip link set dev l3-lanb      down
-ip link del qve-l3-lanb
-brctl delbr qbf-lanb
+ip link set dev l3-lanb down
+ip link del l3-lanb
 
 ## WAN side
-ip link set dev qbf-wanb     down
-ip link set dev qve-l3-wanb  down
-ip link set dev l3-wanb      down
-ip link del qve-l3-wanb 
-brctl delbr qbf-wanb
+ip link set dev l3-wanb down
+ip link del l3-wanb
 
 ## TUN side
-ip link set dev qbf-tunb     down
-ip link set dev qve-l3-tunb  down
-ip link set dev l3-tunb      down
-ip link del qve-l3-tunb
-brctl delbr qbf-tunb
+ip link set dev l3-tunb down
+ip link del l3-tunb
+ovs-vsctl --if-exists del-br qbi-tunb
+
+
+###############################################################################
+# Remove supporting infrastructure for CES-A & CES-B
+###############################################################################
+
+# [COMMON]
+## WAN side
+ip link set dev qbi-wan down
+ip link del qbi-wan
+
+# [CES-A]
+## LAN side
+ip link set dev qbi-lana down
+ip link del qbi-lana
+
+# [CES-B]
+## LAN side
+ip link set dev qbi-lanb down
+ip link del qbi-lanb
+
 
 
 ###############################################################################
@@ -111,7 +70,7 @@ brctl delbr qbf-tunb
 #Create the default namespace
 ln -s /proc/1/ns/net /var/run/netns/default > /dev/null 2> /dev/null
 
-for i in nslana nslanb nswan; do
+for i in nslana nslanb nswan nswan2; do
     ##Remove and create new namespaces
     ip netns del $i > /dev/null 2> /dev/null
 done
