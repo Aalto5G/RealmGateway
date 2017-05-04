767546ff9a4e68c89005f5e0929ff2b67aa74690
Jesus Llorente Santos <jesus.llorente.santos@aalto.fi>
2017-05-04 14:47:00 +0300

    Update bridge AP script. Problem was Windows stripping bytes off from tagged traffic, no MSS clamping needed

--- scripts/bridge_Wifi_AP.sh
+++ scripts/bridge_Wifi_AP.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # Define physical NIC that will be bridged to the WiFi AP
-NIC="enp0s8"
-# Define a maximum MSS_VALUE in case of reduced MTU on the WiFi link
-MSS_VALUE="1360"
-
-# Send traffic from the linux bridge to iptables
-modprobe br_netfilter
-modprobe xt_physdev
-
-sysctl -w net.bridge.bridge-nf-call-arptables=1
-sysctl -w net.bridge.bridge-nf-call-ip6tables=1
-sysctl -w net.bridge.bridge-nf-call-iptables=1
-sysctl -w net.bridge.bridge-nf-filter-pppoe-tagged=0
-sysctl -w net.bridge.bridge-nf-filter-vlan-tagged=1
-sysctl -w net.bridge.bridge-nf-pass-vlan-input-dev=1
+NIC="enp0s25"
 
 # Connect external access point to virtualized CES/RealmGateway demo
 BR_WAN0="br-wan0"
@@ -36,8 +23,3 @@ ip link set dev $NIC.30 up
 ip link set $NIC.10 master $BR_WAN0
 ip link set $NIC.20 master $BR_LANA
 ip link set $NIC.30 master $BR_LANB
-
-# Apply MSS clamping on Linux bridges
-iptables -t mangle -I FORWARD -m physdev --physdev-in  $NIC.+ -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $MSS_VALUE
-iptables -t mangle -I FORWARD -m physdev --physdev-out $NIC.+ -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $MSS_VALUE
-
